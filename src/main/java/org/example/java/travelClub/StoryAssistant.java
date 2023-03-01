fb5637b9578e6b8010886119f9a018346276efbf
RumbleBi <olzlel2000@gmail.com>
2023-03-01 23:57:26 +0900

    java_study

--- src/main/java/org/example/java/travelClub/StoryAssistant.java
+++ src/main/java/org/example/java/travelClub/StoryAssistant.java
@@ -6,7 +6,9 @@ import org.example.java.travelClub.util.DateUtil;
 public class StoryAssistant {
     public static void main(String[] args) {
 //        System.out.println(DateUtil.today());
-        TravelClub newClub = new TravelClub("Jeju Club", "Jeju TravelClub!");
-        String name = newClub.getClubName();
+        TravelClub newClub = new TravelClub("Jeju Club", "Jeju TravelClub Intro");
+        System.out.println(newClub.toString());
+
+        System.out.println(TravelClub.getSample().toString());
     }
 }
--- src/main/java/org/example/java/travelClub/entity/TravelClub.java
+++ src/main/java/org/example/java/travelClub/entity/TravelClub.java
@@ -22,15 +22,24 @@ public class TravelClub {
         // this는 참조하고 있는 레퍼런스 변수를 가르킴. 인스턴스 객체가 여러개일 경우 구분할 수 있는 것.
         this();
         setClubName(clubName);
+        setIntro(intro);
         this.intro = intro;
         this.foundationDay = DateUtil.today();
     }
+
+    /*
+    this 사용 예
+    public TravelClub(String clubName, String intro, String message) {
+        this(clubName, intro);
+        this.message = message;
+    }
+*/
 
     // methods
     // setter
     public void setClubName(String clubName) {
         if(clubName.length() < MIN_NAME_LENGTH) {
-            System.out.println("Club name should be longer then 3");
+            System.out.println("Club name should be longer then " + MIN_NAME_LENGTH);
             return;
         }
         this.clubName = clubName;
@@ -40,11 +49,41 @@ public class TravelClub {
     public String getClubName() {
         return this.clubName;
     }
+    public void setIntro(String intro) {
+        if(intro.length() < MIN_INTRO_LENGTH) {
+            System.out.println("Club intro should be longer then " + MIN_INTRO_LENGTH);
+            return;
+        }
+        this.intro = intro;
+    }
+    public String getIntro() {
+        return this.intro;
+    }
+    public String getId() {
+        return this.id;
+    }
+    public String getFoundationDay() {
+        return this.foundationDay;
+    }
 
-//    this 사용 예
-//    public TravelClub(String clubName, String intro, String message) {
-//        this(clubName, intro);
-//        this.message = message;
-//    }
+    // test code
+    public static TravelClub getSample() {
+        String clubName = "Sample Club";
+        String intro = "Sample Club Intro";
+        TravelClub sampleClub = new TravelClub(clubName, intro);
+        return sampleClub;
 
+//        return new TravelClub("Sample Club", "SampleIntro");
+    }
+
+    // 어노테이션
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("TravelClub club ID: ").append(id);
+        builder.append(", ClubName: ").append(clubName);
+        builder.append(", Intro: ").append(intro);
+        builder.append(", FoundationDay: ").append(foundationDay);
+        return builder.toString();
+    }
 }
