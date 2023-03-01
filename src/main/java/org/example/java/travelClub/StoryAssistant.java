522a4a4eac6b60f4007d916c969bfbc67aa3d58f
RumbleBi <olzlel2000@gmail.com>
2023-03-01 21:30:04 +0900

    java_project

--- src/main/java/org/example/java/travelClub/StoryAssistant.java
+++ src/main/java/org/example/java/travelClub/StoryAssistant.java
@@ -1,7 +1,12 @@
 package org.example.java.travelClub;
 
+import org.example.java.travelClub.entity.TravelClub;
+import org.example.java.travelClub.util.DateUtil;
+
 public class StoryAssistant {
     public static void main(String[] args) {
-
+//        System.out.println(DateUtil.today());
+        TravelClub newClub = new TravelClub("Jeju Club", "Jeju TravelClub!");
+        String name = newClub.getClubName();
     }
 }
--- src/main/java/org/example/java/travelClub/entity/TravelClub.java
+++ src/main/java/org/example/java/travelClub/entity/TravelClub.java
@@ -1,9 +1,13 @@
 package org.example.java.travelClub.entity;
 
+import org.example.java.travelClub.util.DateUtil;
+
 import java.util.UUID;
 
 public class TravelClub {
     // Fields
+    private static final int MIN_NAME_LENGTH = 3;
+    private static final int MIN_INTRO_LENGTH = 10;
     private String id;
     private String clubName;
     private String intro;
@@ -15,9 +19,26 @@ public class TravelClub {
         this.id = UUID.randomUUID().toString();
     }
     public TravelClub(String clubName, String intro) {
+        // this는 참조하고 있는 레퍼런스 변수를 가르킴. 인스턴스 객체가 여러개일 경우 구분할 수 있는 것.
         this();
-        this.clubName = clubName; // 참조하고 있는 레퍼런스 변수를 가르킴. 인스턴스 객체가 여러개일 경우 구분할 수 있는 것.
+        setClubName(clubName);
         this.intro = intro;
+        this.foundationDay = DateUtil.today();
+    }
+
+    // methods
+    // setter
+    public void setClubName(String clubName) {
+        if(clubName.length() < MIN_NAME_LENGTH) {
+            System.out.println("Club name should be longer then 3");
+            return;
+        }
+        this.clubName = clubName;
+    }
+
+    // getter
+    public String getClubName() {
+        return this.clubName;
     }
 
 //    this 사용 예
